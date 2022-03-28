c9d0aa8eea40d65d793b96e45abbfbd5511bbe85
Kɪꜱᴀʀᴀ Pᴇꜱᴀɴᴊɪᴛʜ Pᴇʀᴇʀᴀ 〄 <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2022-03-28 18:27:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -81,7 +81,7 @@ async def start(client, message):
             btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**📮Please Join My Updates Channel to use this Bot!**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
